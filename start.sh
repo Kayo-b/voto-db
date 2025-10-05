ec99062f6badcc152c8e1b112fcaabe07097cfa0
kayo-b <kyo.bhz@gmail.com>
2025-10-05 16:54:53 +0200

    simple start script for backend + fe

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd backend
-python -m uvicorn main:app --host 0.0.0.0 --port ${PORT:-8001}
+cd ~/projects/voto-db/backend && ./venv/bin/python -m uvicorn main_v2:app --host 0.0.0.0 --port 8001 --reload
+cd ~/projects/voto-db/frontend && npm start
